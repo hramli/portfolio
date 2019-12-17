6196d33717fd99a1f549e5c0fb9123089a91c313
Harry Ramli <ramliharry48@yahoo.com>
2019-12-16 23:32:15 -0800

    updated layout and animations

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import profilepic from './../profilepicture.png'
 
 class Home extends React.Component{
     render(){
@@ -6,8 +7,22 @@ class Home extends React.Component{
             <div className="Home container">
                 <div className="outer">
                     <div className="middle">
-                        <h1 className="display-4">Hello, world!</h1>
-                        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>                                         
+                        <div className="row">
+                            <div className="col-12 col-md-6 fade-in-left">
+                                <div className="outer">
+                                    <div className="middle">
+                                        <h1 className="display-4">Hello, world!</h1>
+                                        <p className="lead">I am Harry, a <strong>Computer Science</strong> senior studying at <strong>University of California, Los Angeles</strong></p>                                         
+                                        <p className="lead home-lead-2">Passionate about technology and football.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-12 col-md-6 profile-image-container fade-in-right">
+                                <div className="profile-image">
+                                    <img src={profilepic} alt="profile picture"/>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
