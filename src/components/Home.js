cb477dacaafc3d20e5aafaf3f4cd9ce4c5f56c9c
Harry Ramli <ramliharry48@yahoo.com>
2019-12-17 22:55:56 -0800

    fixed navbar responsiveness

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,23 @@ class Home extends React.Component{
     render(){
         return (
             <div className="Home container">
-                <nav className="navbar fixed-top navbar-light bg-faded fade-in-top">
+                <nav className="navbar fixed-top navbar-dark fade-in-top navbar-expand-md">
                     <div className="container">
-                        <a class="navbar-brand" href="#">Fixed top</a>
+                        <a className="navbar-brand" href="#">HR</a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav ml-auto">
+                                <li className="nav-item active">
+                                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Link</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </nav>
 
